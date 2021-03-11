904d3016d322e4c9eed9a4ec3fba4fdb582d7650
Tara <targol.hosseini@gmail.com>
2021-03-11 23:49:10 +1100

    initial commit

--- main.rb
+++ main.rb
@@ -32,7 +32,13 @@ def current_doctor
     return results[0]
 end
 
+def run_sql(sql, arr = [])
+  db = PG.connect(ENV['DATABASE_URL'] || {dbname: 'clinidocs'})
+  results = db.exec_params(sql, arr)
+  db.close
+  return results
 
+end
 
 
 
